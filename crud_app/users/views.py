ce3c4b6cd9721907b0c5f8bcee5470a784752811
SamEThibault <sam5thibault@gmail.com>
2023-01-08 22:06:16 -0500

    login page complete

--- crud_app/users/views.py
+++ crud_app/users/views.py
@@ -5,11 +5,17 @@ from django.contrib import messages
 # Both GET and POST routes for user registration
 def register(request):
     if request.method == 'POST':
+        # create a form object with the inputs that the user entered
         form = UserCreationForm(request.POST)
+
+        # if everything is valid, show a message and redirect to the home page
         if form.is_valid():
+            form.save()  # save user
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
+        # if it's not a post request, start the form empty
         form = UserCreationForm()
+    # this either renders an empty form for GET requests, or a formed filled with previous entries for invalid POST requests
     return render(request, 'users/register.html', {'form': form})
